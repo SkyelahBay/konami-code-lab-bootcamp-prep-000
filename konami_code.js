60ddcada4fac0ee3135c5e55f4988919feee2fdc
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-19 01:01:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let holder = [];
+  let active = true;
+  document.addEventListener('keydown', function(e){
+    holder.push(e.which);
+  });
+  for(var i = 0; i < holder.length; i++){
+    if(holder[i] !== code[i]){ 
+      /* if the key entered != the same place in code, reset holder to listen again and recall the function */
+      holder.length = 0;
+      return init();
+    }else if(holder === code){
+      alert('you did it!');
+    }
+  }
 
+  
 }
